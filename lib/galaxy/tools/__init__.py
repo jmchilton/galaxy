2906ec20ae3f99861b85f9189fe23bc3c319a7e2
John Chilton <jmchilton@gmail.com>
2015-12-14 10:16:48 +0000

    Check for nodejs and display useful log message if missing...
    ... for loading tool actions that require it.

--- lib/galaxy/tools/__init__.py
+++ lib/galaxy/tools/__init__.py
@@ -75,6 +75,12 @@ JOB_RESOURCE_CONDITIONAL_XML = """<conditional name="__job_resource">
     </when>
 </conditional>"""
 
+REQUIRES_JS_RUNTIME_MESSAGE = ("The tool [%s] requires a nodejs runtime to execute "
+                               "but node nor nodejs could be found on Galaxy's PATH and "
+                               "no runtime was configured using the nodejs_path option in "
+                               "galaxy.ini.")
+
+
 HELP_UNINITIALIZED = threading.Lock()
 
 
@@ -575,6 +581,10 @@ class Tool( object, Dictifiable ):
             module, cls = action
             mod = __import__( module, globals(), locals(), [cls])
             self.tool_action = getattr( mod, cls )()
+            if getattr(self.tool_action, "requires_js_runtime", False):
+                if expressions.find_engine(self.app.config) is None:
+                    message = REQUIRES_JS_RUNTIME_MESSAGE % self.tool_id
+                    raise Exception(message)
         # Tests
         self.__parse_tests(tool_source)
 
@@ -2816,6 +2826,7 @@ class FilterFailedDatasetsTool( DatabaseOperationTool ):
 
 
 class FilterTool( DatabaseOperationTool ):
+    requires_js_runtime = True
     exposed_hda_keys = ['file_size', 'file_ext', 'genome_build']
     tool_type = 'filter_collection'
 
