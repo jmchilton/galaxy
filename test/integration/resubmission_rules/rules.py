2135dec4d953e249f85664be85617e1d6f94b717
John Chilton <jmchilton@gmail.com>
2017-12-06 10:16:57 -0500

    Test resubmit for completely dynamic JobDestination structures.

--- test/integration/resubmission_rules/rules.py
+++ test/integration/resubmission_rules/rules.py
@@ -1,6 +1,17 @@
+from galaxy.jobs import JobDestination
 
 DEFAULT_INITIAL_DESTINATION = "fail_first_try"
 
 
 def initial_destination(resource_params):
     return resource_params.get("initial_destination", None) or DEFAULT_INITIAL_DESTINATION
+
+
+def dynamic_resubmit_once(resource_params):
+    job_destination = JobDestination()
+    job_destination['resubmit'] = [dict(
+        condition="any_failure",
+        destination="local",
+    )]
+    job_destination['runner'] = "failure_runner"
+    return job_destination
