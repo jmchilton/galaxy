4097cc81d42d3d2e83a9959b924fe66218c43312
John Chilton <jmchilton@gmail.com>
2017-12-15 13:55:35 -0500

    Hierarchical upload API optimized for folders & collections.
    Allows describing hierarchical data in JSON or inferring structure from archives or directories.
    
    Datasets or archive sources can be specified via uploads, URLs, paths (if admin && allow_path_paste), library_import_dir/user_library_import_dir, and/or FTP imports. Unlike existing API endpoints, a mix of these on a per file basis is allowed and they work seemlessly between libraries and histories.
    
    Supported "archives" include gzip, zip, bagit directories, bagit achives (with fetching and validations of downloads).
    
    The existing upload API endpoint is quite rough to work with both in terms of adding parameters (e.g. the file type and dbkey hanlding in 4563 was difficult to implement, terribly hacky, and should seemingly have been trivial) and in terms of building requests (one needs to build a tool form - not describe sensible inputs in JSON). This API is built to be intelligable from an API standpoint instead of being constrained to the older style tool form. Additionally it built with hierarchical data in mind in a way that would not be easy at all enhancing the tool form components we don't even render.
    
    This implements 5159 though much simpler YAML descriptions of data libraries should be possible basically as the API descriptions. We can replace the data library script in Ephemeris https://github.com/galaxyproject/ephemeris/blob/master/ephemeris/setup_data_libraries.py with one that converts a simple YAML file into an API call and allows many new options for free.
    
    In future PRs I'll add filtering options to this and it will serve as the backend to 4733.

--- test/base/integration_util.py
+++ test/base/integration_util.py
@@ -7,6 +7,7 @@ tessting configuration.
 import os
 from unittest import skip, TestCase
 
+from galaxy.tools.verify.test_data import TestDataResolver
 from .api import UsesApiTestCaseMixin
 from .driver_util import GalaxyTestDriver
 
@@ -45,6 +46,7 @@ class IntegrationTestCase(TestCase, UsesApiTestCaseMixin):
         cls._app_available = False
 
     def setUp(self):
+        self.test_data_resolver = TestDataResolver()
         # Setup attributes needed for API testing...
         server_wrapper = self._test_driver.server_wrappers[0]
         host = server_wrapper.host
