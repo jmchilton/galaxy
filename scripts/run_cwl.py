738c96f2cd180a845402a93b312034d1781d1410
Hervé  MENAGER <herve.menager@pasteur.fr>
2018-01-20 04:54:18 +0100

    [WIP] modify run_cwl script to make it behave like cwltool
    - display output json like cwltool, as a single indented json
      object (to allow running conformance tests)
    - download data (still needs a lot of work)

--- scripts/run_cwl.py
+++ scripts/run_cwl.py
@@ -2,8 +2,10 @@
 from __future__ import absolute_import, print_function
 
 import argparse
+import json
 import os
 import sys
+import tempfile
 
 from bioblend import galaxy
 
@@ -20,6 +22,32 @@ from base.populators import (  # noqa: I100,I202
 
 DESCRIPTION = """Simple CWL runner script."""
 
+def collect_outputs(cwl_run, output_names, output_directory=None):
+
+    def get_dataset(dataset_details, filename=None):
+        parent_basename = dataset_details.get("cwl_file_name")
+        if not parent_basename:
+            parent_basename = dataset_details.get("name")
+        file_ext = dataset_details["file_ext"]
+        if file_ext == "directory":
+            # TODO: rename output_directory to outputs_directory because we can have output directories
+            # and this is confusing...
+            the_output_directory = os.path.join(output_directory, parent_basename)
+            safe_makedirs(the_output_directory)
+            destination = self.download_output_to(dataset_details, the_output_directory, filename=filename)
+        else:
+            destination = self.download_output_to(dataset_details, output_directory, filename=filename)
+        if filename is None:
+            basename = parent_basename
+        else:
+            basename = os.path.basename(filename)
+        return {"path": destination, "basename": basename}
+
+    outputs = {}
+    for output_name in output_names:
+        cwl_output = cwl_run.get_output_as_object(output_name, download_folder=os.getcwd())
+        outputs[output_name] = cwl_output
+    return outputs
 
 def main(argv=None):
     """Entry point for workflow driving."""
@@ -42,9 +70,12 @@ def main(argv=None):
     tool_source = get_tool_source(tool)
     # TODO: do something with collections at some point
     output_datasets, _ = tool_source.parse_outputs(None)
-    for output_dataset in output_datasets.values():
-        name = output_dataset.name
-        print(run.get_output_as_object(name))
+    output_names = [output_dataset.name for output_dataset in output_datasets.values()]
+    outputs = collect_outputs(run, output_names)
+    print(json.dumps(outputs, indent=4)) 
+    #for output_dataset in output_datasets.values():
+    #    name = output_dataset.name
+    #    print(run.get_output_as_object(name))
 
 
 if __name__ == "__main__":
