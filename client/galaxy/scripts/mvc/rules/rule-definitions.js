30d3bfc34a5f0ef06adb8da78d4a25bab9a719ca
John Chilton <jmchilton@gmail.com>
2018-04-24 11:26:31 -0400

    Rule builder fix - missed allowing collection name in library mode.

--- client/galaxy/scripts/mvc/rules/rule-definitions.js
+++ client/galaxy/scripts/mvc/rules/rule-definitions.js
@@ -659,7 +659,7 @@ const MAPPING_TARGETS = {
         help: _l(
             "If this is set, all rows with the same collection name will be joined into a collection and it is possible to create multiple collections at once."
         ),
-        modes: ["raw", "ftp"], // TODO: allow this in datasets mode.
+        modes: ["raw", "ftp", "library_datasets"], // TODO: allow this in datasets mode.
         importType: "collections"
     },
     name: {
