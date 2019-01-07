jmchilton/galaxy

commit cb7ebd73494153b1950ec87a39d12e874c183a13

py3: Fix history import/export tool
 - Open history tarfile in binary mode
 - Fix base64 encoding in history import tool
 - test history export more extensively

lib/galaxy/tools/imp_exp/export_history.py
@@ -25,7 +25,7 @@ def get_dataset_filename(name, ext, hid):
 
 
 def create_archive(history_attrs_file, datasets_attrs_file, jobs_attrs_file, out_file, gzip=False):
-    """ Create archive from the given attribute/metadata files and save it to out_file. """
+    """Create archive from the given attribute/metadata files and save it to out_file."""
     tarfile_mode = "w"
     if gzip:
         tarfile_mode += ":gz"
@@ -34,17 +34,16 @@ def create_archive(history_attrs_file, datasets_attrs_file, jobs_attrs_file, out
         history_archive = tarfile.open(out_file, tarfile_mode)
 
         # Read datasets attributes from file.
-        datasets_attr_in = open(datasets_attrs_file, 'rb')
-        datasets_attr_str = ''
-        buffsize = 1048576
-        try:
-            while True:
-                datasets_attr_str += datasets_attr_in.read(buffsize)
-                if not datasets_attr_str or len(datasets_attr_str) % buffsize != 0:
-                    break
-        except OverflowError:
-            pass
-        datasets_attr_in.close()
+        with open(datasets_attrs_file, 'rb') as datasets_attr_in:
+            datasets_attr_str = ''
+            buffsize = 1048576
+            try:
+                while True:
+                    datasets_attr_str += datasets_attr_in.read(buffsize).decode('utf-8')
+                    if not datasets_attr_str or len(datasets_attr_str) % buffsize != 0:
+                        break
+            except OverflowError:
+                pass
         datasets_attrs = loads(datasets_attr_str)
 
         # Add datasets to archive and update dataset attributes.
@@ -78,9 +77,8 @@ def create_archive(history_attrs_file, datasets_attrs_file, jobs_attrs_file, out
                 dataset_attrs['file_name'] = dataset_archive_name
 
         # Rewrite dataset attributes file.
-        datasets_attrs_out = open(datasets_attrs_file, 'w')
-        datasets_attrs_out.write(dumps(datasets_attrs))
-        datasets_attrs_out.close()
+        with open(datasets_attrs_file, 'w') as datasets_attrs_out:
+            datasets_attrs_out.write(dumps(datasets_attrs))
 
         # Finish archive.
         history_archive.add(history_attrs_file, arcname="history_attrs.txt")

lib/galaxy/tools/imp_exp/imp_history_from_archive.xml
@@ -1,7 +1,11 @@
 <tool id="__IMPORT_HISTORY__" name="Import History" version="0.1" tool_type="import_history">
   <type class="ImportHistoryTool" module="galaxy.tools"/>
   <action module="galaxy.tools.actions.history_imp_exp" class="ImportHistoryToolAction"/>
-  <command>python '$__tool_directory__/unpack_tar_gz_archive.py' '${ str( $__ARCHIVE_SOURCE__ ).encode( 'base64' ) }' '${ str( $__DEST_DIR__ ).encode( 'base64' ) }' --$__ARCHIVE_TYPE__ --encoded</command>
+  <command>#from base64 import b64encode#
+python '$__tool_directory__/unpack_tar_gz_archive.py'
+'${ b64encode(bytearray(str($__ARCHIVE_SOURCE__), encoding="utf-8")).decode('utf-8')}'
+'${ b64encode(bytearray(str($__DEST_DIR__), encoding="utf-8")).decode('utf-8')}'
+--$__ARCHIVE_TYPE__ --encoded</command>
   <inputs>
     <param name="__ARCHIVE_SOURCE__" type="text">
       <sanitizer sanitize="False"/>

lib/galaxy/tools/imp_exp/unpack_tar_gz_archive.py
@@ -75,8 +75,8 @@ def main(options, args):
     archive_source, dest_dir = args
 
     if options.is_b64encoded:
-        archive_source = b64decode(archive_source)
-        dest_dir = b64decode(dest_dir)
+        archive_source = b64decode(archive_source).decode('utf-8')
+        dest_dir = b64decode(dest_dir).decode('utf-8')
 
     # Get archive from URL.
     if is_url:
@@ -93,7 +93,7 @@ if __name__ == "__main__":
     # Parse command line.
     parser = optparse.OptionParser()
     parser.add_option('-U', '--url', dest='is_url', action="store_true", help='Source is a URL.')
-    parser.add_option('-F', '--file', dest='is_file', action="store_true", help='Source is a URL.')
+    parser.add_option('-F', '--file', dest='is_file', action="store_true", help='Source is a file.')
     parser.add_option('-e', '--encoded', dest='is_b64encoded', action="store_true", default=False, help='Source and destination dir values are base64 encoded.')
     (options, args) = parser.parse_args()
     try:

lib/galaxy/web/base/controller.py
@@ -394,7 +394,8 @@ class ExportsHistoryMixin(object):
             trans.response.set_content_type('application/x-tar')
         disposition = 'attachment; filename="%s"' % jeha.export_name
         trans.response.headers["Content-Disposition"] = disposition
-        return open(trans.app.object_store.get_filename(jeha.dataset))
+        archive = trans.app.object_store.get_filename(jeha.dataset)
+        return open(archive, mode='rb')
 
     def queue_history_export(self, trans, history, gzip=True, include_hidden=False, include_deleted=False):
         # Convert options to booleans.

test/unit/tools/test_history_imp_exp.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import tarfile
 from shutil import rmtree
 from tempfile import mkdtemp
@@ -6,6 +7,7 @@ from tempfile import mkdtemp
 from galaxy import model
 from galaxy.exceptions import MalformedContents
 from galaxy.tools.imp_exp import JobImportHistoryArchiveWrapper, unpack_tar_gz_archive
+from galaxy.tools.imp_exp.export_history import create_archive
 from ..unittest_utils.galaxy_mock import MockApp
 
 
@@ -30,6 +32,29 @@ def _run_jihaw_cleanup(history_archive, msg):
         assert data != 'insecure', msg
     except MalformedContents:
         pass
+
+
+def test_create_archive():
+    tempdir = mkdtemp()
+    dataset = os.path.join(tempdir, 'dataset_1.dat')
+    history_attrs_file = os.path.join(tempdir, 'history_attrs_file.txt')
+    datasets_attrs_file = os.path.join(tempdir, 'dataset_attrs_file.txt')
+    jobs_attrs_file = os.path.join(tempdir, 'jobs_attrs_file.txt')
+    out_file = os.path.join(tempdir, 'out.tar.gz')
+    with open(dataset, 'w') as out:
+        out.write('Hello\n')
+    with open(history_attrs_file, 'w') as out:
+        out.write(HISTORY_ATTRS)
+    with open(datasets_attrs_file, 'w') as out:
+        out.write(DATASETS_ATTRS.format(file_name=dataset))
+    with open(jobs_attrs_file, 'w') as out:
+        out.write(JOBS_ATTRS)
+    try:
+        create_archive(history_attrs_file, datasets_attrs_file, jobs_attrs_file, out_file, gzip=True)
+        with tarfile.open(out_file) as t:
+            assert t.getnames() == ['datasets/Pasted_Entry_1.txt', 'history_attrs.txt', 'datasets_attrs.txt', 'jobs_attrs.txt']
+    finally:
+        shutil.rmtree(tempdir)
 
 
 def test_history_import_symlink():

commit 442adf5c65ba4c25d91055ca734d1106604b3ff5

Fix export_history.py for large exports?
Broken in cb7ebd73494153b1950ec87a39d12e874c183a13 - notice re-initializing that string.

lib/galaxy/tools/imp_exp/export_history.py
@@ -35,16 +35,7 @@ def create_archive(history_attrs_file, datasets_attrs_file, jobs_attrs_file, out
 
         # Read datasets attributes from file.
         with open(datasets_attrs_file) as datasets_attr_in:
-            datasets_attr_str = ''
-            buffsize = 1048576
-            try:
-                while True:
-                    datasets_attr_str += datasets_attr_in.read(buffsize)
-                    if not datasets_attr_str or len(datasets_attr_str) % buffsize != 0:
-                        break
-            except OverflowError:
-                pass
-        datasets_attrs = loads(datasets_attr_str)
+            datasets_attrs = load(dataset_attrs_in)
 
         # Add datasets to archive and update dataset attributes.
         # TODO: security check to ensure that files added are in Galaxy dataset directory?