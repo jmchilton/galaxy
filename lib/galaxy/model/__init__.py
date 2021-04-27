0f23be87f95a47a0522230a81e7a571dd42a57ff
John Chilton <jmchilton@gmail.com>
2021-04-27 17:23:52 -0400

    [WIP] Add user favorite extensions.

--- lib/galaxy/model/__init__.py
+++ lib/galaxy/model/__init__.py
@@ -6591,6 +6591,11 @@ class UserPreference(RepresentById):
         self.value = value
 
 
+class UserFavoriteExtension(RepresentById):
+    def __init__(self, value=None):
+        self.value = value
+
+
 class UserAction(RepresentById):
     def __init__(self, id=None, create_time=None, user_id=None, session_id=None, action=None, params=None, context=None):
         self.id = id
