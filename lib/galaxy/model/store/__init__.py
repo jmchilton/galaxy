c5fc5f9117d4b78cece85474df422ec70565d126
John Chilton <jmchilton@gmail.com>
2021-09-23 09:34:27 -0400

    Invocations in import dict store.

--- lib/galaxy/model/store/__init__.py
+++ lib/galaxy/model/store/__init__.py
@@ -68,6 +68,7 @@ DICT_STORE_ATTRS_KEY_COLLECTIONS = "collections"
 DICT_STORE_ATTRS_KEY_JOBS = "jobs"
 DICT_STORE_ATTRS_KEY_IMPLICIT_COLLECTION_JOBS = "implicit_collection_jobs"
 DICT_STORE_ATTRS_KEY_LIBRARIES = "libraries"
+DICT_STORE_ATTRS_KEY_INVOCATIONS = "invocations"
 
 
 class ImportOptions:
@@ -1138,6 +1139,12 @@ class DictImportModelStore(ModelImportStore):
 
     def implicit_collection_jobs_properties(self):
         return self._store_as_dict.get(DICT_STORE_ATTRS_KEY_IMPLICIT_COLLECTION_JOBS) or []
+
+    def invocations_properties(self):
+        return self._store_as_dict.get(DICT_STORE_ATTRS_KEY_INVOCATIONS) or []
+
+    def workflow_paths(self):
+        return []
 
 
 def get_import_model_store_for_dict(as_dict, **kwd):
