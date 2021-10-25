57901182a24b698c43ab88a2fa53f48d2c39bf9a
John Chilton <jmchilton@gmail.com>
2021-10-25 10:40:01 -0400

    Implement task to generate PDFs and stash them into short term storage for download.

--- lib/galaxy/schema/tasks.py
+++ lib/galaxy/schema/tasks.py
@@ -1,6 +1,15 @@
 from pydantic import BaseModel
 
+from ..schema import PdfDocumentType
+
 
 class PrepareDatasetCollectionDownload(BaseModel):
     short_term_storage_request_id: str
     history_dataset_collection_association_id: int
+
+
+class GeneratePdfDownload(BaseModel):
+    short_term_storage_request_id: str
+    # basic markdown - Galaxy directives need to be processed before handing off to this task
+    basic_markdown: str
+    document_type: PdfDocumentType
