dbeb6fc5b6eeaae99735b585f8e8ed87c1c8f60a
John Chilton <jmchilton@gmail.com>
2022-06-09 10:33:37 -0400

    private objectstores & dataset.sharable
    Setup abstractions to prevent sharing transient or private per-user objects in an objectstore.

--- lib/galaxy/jobs/__init__.py
+++ lib/galaxy/jobs/__init__.py
@@ -1589,6 +1589,8 @@ class MinimalJobWrapper(HasResourceParameters):
 
         object_store_populator = ObjectStorePopulator(self.app, job.user)
         object_store_id = self.get_destination_configuration("object_store_id", None)
+        require_sharable = job.requires_sharable_storage(self.app.security_agent)
+
         if object_store_id:
             object_store_populator.object_store_id = object_store_id
 
@@ -1600,7 +1602,7 @@ class MinimalJobWrapper(HasResourceParameters):
         # afterward. State below needs to happen the same way.
         for dataset_assoc in job.output_datasets + job.output_library_datasets:
             dataset = dataset_assoc.dataset
-            object_store_populator.set_object_store_id(dataset)
+            object_store_populator.set_object_store_id(dataset, require_sharable=require_sharable)
 
         job.object_store_id = object_store_populator.object_store_id
         self._setup_working_directory(job=job)
