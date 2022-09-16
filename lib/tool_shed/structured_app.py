600a86dd3dfe1cdfbb8e2d2bfff1a424ca017c19
John Chilton <jmchilton@gmail.com>
2022-09-16 14:28:42 -0400

    Rebase into tool shed app...

--- lib/tool_shed/structured_app.py
+++ lib/tool_shed/structured_app.py
@@ -4,7 +4,9 @@ from galaxy.structured_app import BasicSharedApp
 
 if TYPE_CHECKING:
     from tool_shed.repository_types.registry import Registry as RepositoryTypesRegistry
+    from tool_shed.webapp.model import mapping
 
 
 class ToolShedApp(BasicSharedApp):
     repository_types_registry: "RepositoryTypesRegistry"
+    model: "mapping.ToolShedModelMapping"
