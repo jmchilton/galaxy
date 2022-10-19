fef50edb33bc052509e4f388d2284f547732134f
John Chilton <jmchilton@gmail.com>
2022-10-19 09:10:01 -0400

    Standalone implementation of tool shed client...

--- lib/galaxy/tool_shed/unittest_utils/__init__.py
+++ lib/galaxy/tool_shed/unittest_utils/__init__.py
@@ -24,6 +24,7 @@ from galaxy.tool_shed.galaxy_install.client import (
 )
 from galaxy.tool_shed.util.repository_util import get_installed_repository
 from galaxy.tool_util.data import ToolDataTableManager
+from galaxy.tool_util.loader_directory import looks_like_a_tool
 from galaxy.tool_util.toolbox.base import AbstractToolBox
 from galaxy.tool_util.toolbox.watcher import (
     get_tool_conf_watcher,
@@ -66,6 +67,9 @@ class Config:
     shed_tool_data_table_config: str
     shed_data_manager_config_file: str
 
+    def get(self, key, default):
+        return getattr(self, key, default)
+
 
 class TestTool:
     _macro_paths: List[str] = []
@@ -102,6 +106,9 @@ class TestToolBox(AbstractToolBox):
             installed_changeset_revision=installed_changeset_revision,
             from_cache=True,
         )
+
+    def _looks_like_a_tool(self, path):
+        return looks_like_a_tool(path, enable_beta_formats=False)
 
 
 class Watchers:
@@ -202,6 +209,12 @@ class StandaloneInstallationTarget(InstallationTarget):
             config_filename=self.config.shed_tool_data_table_config,
             other_config_dict=self.config,
         )
+        dependency_dir = target_directory / "_dependencies"
+        dependency_dir.mkdir()
+
+    @property
+    def tool_dependency_dir(self) -> Optional[str]:
+        return None
 
     def reload_toolbox(self):
         self._toolbox = TestToolBox(
