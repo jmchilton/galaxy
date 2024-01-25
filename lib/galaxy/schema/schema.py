e58ffb7440ffc182e8596b3eae4350dfd623c370
John Chilton <jmchilton@gmail.com>
2024-01-25 12:16:25 -0500

    Input parameter schema.

--- lib/galaxy/schema/schema.py
+++ lib/galaxy/schema/schema.py
@@ -1225,6 +1225,7 @@ class JobIndexQueryPayload(Model):
     history_id: Optional[DecodedDatabaseIdField] = None
     workflow_id: Optional[DecodedDatabaseIdField] = None
     invocation_id: Optional[DecodedDatabaseIdField] = None
+    tool_request_id: Optional[DecodedDatabaseIdField] = None
     order_by: JobIndexSortByEnum = JobIndexSortByEnum.update_time
     search: Optional[str] = None
     limit: int = 500
@@ -3386,6 +3387,16 @@ class AsyncTaskResultSummary(Model):
         None,
         title="Queue of task being done derived from Celery AsyncResult",
     )
+
+
+ToolRequestIdField = Field(title="ID", description="Encoded ID of the role")
+
+
+class ToolRequestModel(Model):
+    id: DecodedDatabaseIdField = ToolRequestIdField
+    request: Dict[str, Any]
+    state: str
+    state_message: Optional[str]
 
 
 class AsyncFile(Model):
