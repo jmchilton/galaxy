b9cef3d0e4980e6d418ec15ccb3e22c74c339076
John Chilton <jmchilton@gmail.com>
2024-11-12 10:41:34 -0500

    Type fixes?

--- lib/galaxy/tool_util/deps/mulled/mulled_hash.py
+++ lib/galaxy/tool_util/deps/mulled/mulled_hash.py
@@ -10,17 +10,26 @@ Produce a mulled hash with:
 
     mulled-hash samtools=1.3.1,bedtools=2.22
 """
+from typing import (
+    Callable,
+    Dict,
+    Iterable,
+)
 
 from ._cli import arg_parser
-from ..conda_util import hash_conda_packages
+from ..conda_util import (
+    CondaTarget,
+    hash_conda_packages,
+)
 from .mulled_build import target_str_to_targets
 from .util import (
     v1_image_name,
     v2_image_name,
 )
 
+HASH_FUNCTION = Callable[[Iterable[CondaTarget], ...], str]
 
-IMAGE_FUNCS = {
+IMAGE_FUNCS: Dict[str, HASH_FUNCTION] = {
     "conda": lambda x: f"mulled-v1-{hash_conda_packages(x)}",
     "v1": v1_image_name,
     "v2": v2_image_name,
