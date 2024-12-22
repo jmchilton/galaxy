366f5da708b7048fb09f8a1e1681cac60f68ae57
John Chilton <jmchilton@gmail.com>
2024-12-22 15:08:23 -0500

    Implement list building wizard.

--- client/src/components/Collections/ListCollectionCreator.vue
+++ client/src/components/Collections/ListCollectionCreator.vue
@@ -28,6 +28,7 @@ interface Props {
     defaultHideSourceItems?: boolean;
     fromSelection?: boolean;
     extensions?: string[];
+    showButtons?: boolean;
 }
 
 const props = defineProps<Props>();
@@ -397,6 +398,7 @@ function renameElement(element: any, name: string) {
                 collection-type="list"
                 :no-items="props.initialElements.length == 0 && !props.fromSelection"
                 :show-upload="!fromSelection"
+                :show-buttons="showButtons"
                 @add-uploaded-files="addUploadedFiles"
                 @on-update-datatype-toggle="changeDatatypeFilter"
                 @onUpdateHideSourceItems="onUpdateHideSourceItems"
@@ -419,8 +421,8 @@ function renameElement(element: any, name: string) {
 
                     <ul>
                         <li v-if="!fromSelection">
-                            Move datasets from the "Unselected" column to the "Selected" column below to compose the list
-                            in the intended order and with the intended datasets.
+                            Move datasets from the "Unselected" column to the "Selected" column below to compose the
+                            list in the intended order and with the intended datasets.
                         </li>
                         <li v-if="!fromSelection">
                             The filter textbox can be used to rapidly find the datasets of interest by name.
