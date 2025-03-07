97fe78bb5dcc525b2acfae42909555b2eb30c55c
John Chilton <jmchilton@gmail.com>
2025-03-06 20:21:57 -0500

    [WIP] Implement records - heterogenous dataset collections.
    Existing dataset colleciton types are meant to be homogenous - all datasets of the same time. This introduces CWL-style record dataset collections.

--- client/src/api/schema/schema.ts
+++ client/src/api/schema/schema.ts
@@ -6999,6 +6999,12 @@ export interface components {
              */
             element_identifiers?: components["schemas"]["CollectionElementIdentifier"][] | null;
             /**
+             * Fields
+             * @description List of fields to create for this collection. Set to 'auto' to guess fields from identifiers.
+             * @default []
+             */
+            fields: string | components["schemas"]["FieldDict"][] | null;
+            /**
              * Folder Id
              * @description The ID of the library folder that will contain the collection. Required if `instance_type=library`.
              */
@@ -7190,6 +7196,12 @@ export interface components {
              * @description List of elements that should be in the new collection.
              */
             element_identifiers?: components["schemas"]["CollectionElementIdentifier"][] | null;
+            /**
+             * Fields
+             * @description List of fields to create for this collection. Set to 'auto' to guess fields from identifiers.
+             * @default []
+             */
+            fields: string | components["schemas"]["FieldDict"][] | null;
             /**
              * Folder Id
              * @description The ID of the library folder that will contain the collection. Required if `instance_type=library`.
@@ -9154,6 +9166,17 @@ export interface components {
             hash_function: "MD5" | "SHA-1" | "SHA-256" | "SHA-512";
             /** Hash Value */
             hash_value: string;
+        };
+        /** FieldDict */
+        FieldDict: {
+            /** Format */
+            format?: string | null;
+            /** Name */
+            name: string;
+            /** Type */
+            type:
+                | ("File" | "null" | "boolean" | "int" | "float" | "string")
+                | ("File" | "null" | "boolean" | "int" | "float" | "string")[];
         };
         /** FileDataElement */
         FileDataElement: {
