7498cc8c9b6da367610c25825b667a1e8948de4b
John Chilton <jmchilton@gmail.com>
2025-06-25 09:25:09 -0400

    Tool Request API...

--- lib/galaxy/managers/tools.py
+++ lib/galaxy/managers/tools.py
@@ -21,6 +21,7 @@ from galaxy import (
     exceptions,
     model,
 )
+from galaxy.managers.context import ProvidesUserContext
 from galaxy.model import (
     DynamicTool,
     UserDynamicToolAssociation,
