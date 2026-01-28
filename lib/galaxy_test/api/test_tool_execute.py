752dc33de2936134eec5e9e26a27dab5aa7b01f6
John Chilton <jmchilton@gmail.com>
2026-01-28 10:29:26 -0500

    More linting fixes...

--- lib/galaxy_test/api/test_tool_execute.py
+++ lib/galaxy_test/api/test_tool_execute.py
@@ -693,9 +693,7 @@ def test_select_multiple_does_default_to_select_values_marked_as_selected(
 
 
 @requires_tool_id("gx_select_multiple_one_default_user")
-def test_select_multiple_does_default_to_select_values_marked_as_selected_user(
-    required_tool: RequiredTool
-):
+def test_select_multiple_does_default_to_select_values_marked_as_selected_user(required_tool: RequiredTool):
     required_tool.execute.with_request({}).assert_has_single_job.with_output("output").with_contents_stripped(
         '["--ex3"]'
     )
